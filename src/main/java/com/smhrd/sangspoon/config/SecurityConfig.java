58177e4e0ed98a83fa9fc1cf73580e26861831e7
tnvs99 <tnvs99@gmail.com>
2025-07-22 19:01:55 +0900

    add: SecurityConfig(비활성화)

---
+++ src/main/java/com/smhrd/sangspoon/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.smhrd.sangspoon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // 일단은 모든 보안기능(?) 비활성화
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .build();
+    }
+}
